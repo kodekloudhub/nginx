3c6b870fc0e3c93ea1818ad3b82d5967d5349808
Jeff Silverman <jeffsilverm@gmail.com>
2024-02-12 01:54:33 -0800

    First commit

---
+++ www/html/ru/page_header.js
@@ -0,0 +1,3 @@
+document.writeln("This document was last modified "+document.lastModified  + 
+	"  The URL is: " + window.location.href + 
+	"  The page's pathname is: " + window.location.pathname );
