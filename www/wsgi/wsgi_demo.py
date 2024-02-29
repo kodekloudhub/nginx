cb1f32a5254b5901b52e68c02802fbf7ff53be35
Jeff Silverman <jeffsilverm@gmail.com>
2024-02-29 09:58:09 -0800

    A moderately major re-write.
    More exhaustive testing has been done, which has discovered another failure mode (the int
    call on line 42 to find the content_length).  The scope of the try statement has been
    reduced.  If the HTTP header is missing the CONTENT_LENGTH field, then the program will
    get a sane value from the parameters in the header.  It isn't obvious to me that this is
    still using the WSGI protocol, but it works.

--- www/wsgi/wsgi_demo.py
+++ www/wsgi/wsgi_demo.py
@@ -1,7 +1,17 @@
 #! /usr/bin/env python3
+#
+# This is a simple WSGI application.  It should be started under a web server such as gunicorn.
+#
 import sys
 from wsgiref.simple_server import make_server
-from urllib.parse import parse_qs
+
+# For reasons I do not understand, output to stderr does not appear.  If I figure out the reason and can fix it, then
+# change ERROR_OUT to sys.stderr
+ERROR_OUT = sys.stdout
+PORT = 8000  # Listen on this port.  Traditionally, somewhere between 8000 and 8999.
+# It is a bad idea to use a port number less than 1024, because that requires root priveleges
+# and if you need root, then you are doing something wrong.
+
 
 def application(environ, start_response):
     status = '200 OK'
@@ -27,30 +37,39 @@ def application(environ, start_response):
     """
 
     if environ['REQUEST_METHOD'] == 'POST':
+        print(f"The user agent is {environ['HTTP_USER_AGENT']}", file=ERROR_OUT)
         try:
-            print(f"The user agent is {environ['HTTP_USER_AGENT']}")
             content_length = int(environ.get('CONTENT_LENGTH', 0))
-            # I am having problems with httpx, which I think is passing ill-formed queries, specifically
-            # explicitly setting the content length to 0.  This happens when I invoke httpx from the command line.
-            # I have not tested httpx when called programmatically. (as of 2024-02-28)
-            if content_length > 0:
-                print(f"Content length is {content_length}")
-            else:
-                query_str = environ.get("QUERY_STRING")
-                query_string_len = len(query_str)
-                print (f"Replacing CONTENT-LENGTH, which is 0, with the length of the query string, which is {query_string_len}")
+        except ValueError:
+            print(f"environ['CONTENT_LENGTH'] exists but is not a valid integer {environ['CONTENT_LENGTH']}.  Using 0",
+                  file=ERROR_OUT )
+            content_length = 0
+        # I am having problems with httpx and curl, which I think is passing ill-formed queries, specifically
+        # explicitly setting the content length to 0.  This happens when I invoke httpx from the command line.
+        # I have not tested httpx when called programmatically. (as of 2024-02-28).  This is not an issue with
+        # Google Chrome or chromium.
+        if content_length > 0:
+            print(f"Content length is {content_length}")
             post_data = environ['wsgi.input'].read(content_length).decode('utf-8')
-            if len(post_data) == 0:
-                post_data = query_str
-            print(f"post_data is {post_data}\n", file=sys.stdout)
-            data = parse_post_data(post_data)
+        else:
+            post_data = environ["QUERY_STRING"]
+            content_length = len(post_data)
+            print(f"Replacing CONTENT-LENGTH, which is 0, with the length of QUERY_STRING, which is {content_length}")
+        if len(post_data) == 0:
+            print(f"The length of post_data is 0, should be longer.  Query_str is {post_data}"
+                  f"content_length is {content_length}, environ is: ", file=ERROR_OUT)
+            for key in environ:
+                print(f"{key}: {environ[key]}", file=ERROR_OUT)
+            raise AssertionError  # This seems overly draconian
+        print(f"post_data is {post_data}\n", file=ERROR_OUT)
+        data = parse_post_data(post_data)
+        try:
             num1 = int(data['num1'])
             num2 = int(data['num2'])
-            result = num1 + num2
         except (KeyError, ValueError):
             result = f'Error: Please enter valid numbers. <b>post_data</b> is {post_data} '
-            # <b>environ</b> is "' \
-            #         f'{environ}'
+        else:
+            result = num1 + num2
     else:
         result = ''
 
@@ -66,8 +85,10 @@ def parse_post_data(post_data):
     return data
 
 
-# Creating a server
-with make_server('', 8000, application) as httpd:
+# Creating a server that listens on TCP port PORT.
+# When a web client makes a query, the server will call
+# application which then does whatever is needed.
+with make_server('', PORT, application) as httpd:
     print("Serving on port 8000...")
     # Serve until process is killed
     httpd.serve_forever()
