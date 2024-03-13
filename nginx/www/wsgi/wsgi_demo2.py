42d53ce1a33415f2ace5af11ad9dd45c73195b11
Jeff Silverman <jeffsilverm@gmail.com>
2024-03-13 00:49:05 -0700

    Passes all test cases where the client browser and the server are on the same machine.
    Thus:
    
    Get the form to fill in.  This is normally the first step.
    httpx -m GET http://localhost:8023
    Send the form back for processing and using valid integers  This is normally the second step.
    httpx -m POST -p NUM1 34 -p NUM2 16 http://localhost:8023
    After producing an answer, does the server continue to work?
    httpx -m GET http://localhost:8023
    This is the first test of a failure mode: in invalid integer.
    httpx -m POST -p NUM1 34.2 -p NUM2 16 http://localhost:8023
    Does the server continue to work
    httpx -m POST -p NUM1 24 -p NUM2 16 http://localhost:8023?
    Test with an invalid method
    httpx -m PUT -p NUM1 24 -p NUM2 16 http://localhost:8023
    Is the server still working?
    httpx -m GET http://localhost:8023

--- nginx/www/wsgi/wsgi_demo2.py
+++ nginx/www/wsgi/wsgi_demo2.py
@@ -2,48 +2,60 @@
 #
 #
 from wsgiref.simple_server import make_server
+PORT = 8023
 
+
+# application is called by the WSGI middleware
 def application(environ, start_response):
-    status = '200 OK'
     headers = [('Content-type', 'text/html; charset=utf-8')]
-    start_response(status, headers)
-
-    html = """
-    <html>
-    <head>
-        <title>Addition Form</title>
-    </head>
-    <body>
-        <h1>Addition Form</h1>
-        <form action="" method="get">
-            <label for="num1">Enter the first number:</label><br>
-            <input type="text" id="num1" name="num1"><br>
-            <label for="num2">Enter the second number:</label><br>
-            <input type="text" id="num2" name="num2"><br><br>
-            <input type="submit" value="Calculate Sum">
-        </form>
-        {}
-    </body>
-    </html>
-    """
-
-    if environ['QUERY_STRING']:
+    if "GET" == environ['REQUEST_METHOD']:
+        html = """
+        <html>
+        <head>
+            <title>Addition Form</title>
+        </head>
+        <body>
+            <h1>Addition Form</h1>
+            <form action="" method="POST">
+                <label for="num1">Enter the first number:</label><br>
+                <input type="text" id="num1" name="num1"><br>
+                <label for="num2">Enter the second number:</label><br>
+                <input type="text" id="num2" name="num2"><br><br>
+                <input type="submit" value="Calculate Sum">
+            </form>
+        <p>Thank you</p>
+        </body>
+        </html>
+        """
+        status = "200 OK"
+    elif "POST" == environ['REQUEST_METHOD']:
         query_string = environ['QUERY_STRING']
+        num1 = num2 = None
         try:
             num1 = int(query_string.split('&')[0].split('=')[1])
             num2 = int(query_string.split('&')[1].split('=')[1])
             sum_result = num1 + num2
-            result_html = "<p>Sum of {} and {} is {}</p>".format(num1, num2, sum_result)
-            return [html.format(result_html).encode()]
+            html = "<html><head><title>Addition results</title></head>" \
+                   f"<body><p>Sum of {num1} and {num2} is {sum_result}</p></body></html>"
+            status = '200 OK'
+
         except ValueError:
-            error_html = "<p>Error: Please enter valid integer values for both numbers.</p>"
-            return [html.format(error_html).encode()]
+            status = "400 Bad Request"
+            html = "<html><head><title>Addition results</title></head>" \
+                   f"<p>Error: Please enter valid integer values for both numbers {num1} {num2}.</p></body></html>"
     else:
-        return [html.format("").encode()]
+        status = "405 Method not allowed"
+        html = "<html><head><title>Addition results</title></head>" \
+               f"<p>Error: The only methods allowed are GET and POST.  You entered {environ['REQUEST_METHOD']}</p>" \
+               "</body> </html>"
+        print(html, "\n\n")
+# calling start_response should be done only when the response is complete.  Otherwise, one runs the risk of calling
+# start_response twice, and that raises an exception within wsgiref.  Also, calling it is DRY.
+    start_response(status, headers)
+    return [html.encode()]
+
 
 if __name__ == '__main__':
-    with make_server('', 8000, application) as httpd:
-        print("Serving on port 8000...")
+    with make_server('', PORT, application) as httpd:
+        print(f"Serving on port {PORT}...")
         httpd.serve_forever()
-
-
