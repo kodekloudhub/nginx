73731e264e5746203e5ddea6d7cbb51a3a8222bb
Jeff Silverman <jeffsilverm@gmail.com>
2024-03-20 02:59:12 -0700

    App.py works with GET (creates a form, which works), POST (either with arguments passed through the URL or in the data).
    PUT does not work yet, raises a 405 Method Not Allowed status return.
    
    Dig into that tomorrow.

--- nginx/configurations/module_4_APIs/app/app.py
+++ nginx/configurations/module_4_APIs/app/app.py
@@ -1,35 +1,74 @@
-from flask import Flask, render_template, request, jsonify
+#! /usr/bin/env python3
+#
+#
+import sys
+from datetime import datetime as dt
 
+from flask import Flask, render_template, request, jsonify, make_response
+from flask_debugtoolbar import DebugToolbarExtension
+
+LOGFILENAME = "flask_log_"
 app = Flask(__name__)
 
 # Sample data
-items = []
+items = {}
 
 
 @app.route('/')
 def index():
-    return html, 200  # html is defined just before call app.run, below.  I did that to make this method easy to understand
+    log_something("Created the HTML file")
+    return html, 200  # Variable html is defined before call app.run, below.
+    # I did that to make this method easy to understand
 
 
 # API endpoint for listing items
 @app.route('/api/items', methods=['GET'])
 def get_items():
-    # jsonify, a.k.a. flask.json.jsonify  returns a Response object with MIME type application/json
-    return jsonify(items)
+    r = make_response((items, 200, {"Content-Type": "application/json"}))
+    return r
 
 
 # API endpoint for creating an item
 @app.route('/api/items', methods=['POST'])
 def create_item():
-    data = request.json
-    items.append(data)
-    return jsonify(data), 201  # This must be 201 (Created) and not 200
+    # request.form returns parsed form data from a PUT or POST operation
+    data = request.form
+    # I think something is wrong with the way I am calling nginx using httpx.  Using the form works.
+    # Both
+    # httpx -v -m POST -d key key_7000 -d value value_7000 -h Content-Type application/json http://e7240/api/items
+    # and
+    # httpx -v -m POST -p key key_7000 -p value value_7000 -h Content-Type application/json http://e7240/api/items
+    # Cause a failure.
+    if data.get(key='key', default=None) is None:
+        data = request.args     # POST should not put arguments in the URL
+        if data.get(key='key', default=None) is None:
+            return "Can't find the arguments.  " \
+                "This might be a problem with the client, might be a problem with the server", 500
+        else:
+            print(f"Got the arguments from the URL after all")
+    # data is a werkzeug.datastructures.structures.ImmutableMultiDict which is an immutable MultiDict.
+    print(f"the keys in data are {data.keys()},\n data is {data}\n", file=sys.stderr)
+    sys.stderr.flush()
+
+    key = data.get(key='key', default=None)
+    value = data.get(key='key', default=None)
+    if key is None or value is None:
+        return f"Having troubles decoding the query.  key is {key} and value is {value}", 500
+
+    print(f"Value for key is {key} and value is: {value}")
+    items[key] = value
+    log_something(f"in create_item:  data is {data}  data.keys() is {data.keys()}, items is now {items}.")
+    # status must be 201 (Created) and not 200 (Okay)
+    # See https://tedboy.github.io/flask/interface_api.application_object.html?highlight=make_response#flask.Flask.make_response    # noqa
+    r = make_response((items, 201, {"Content-Type": "application/json"}))
+    return r
 
 
 # API endpoint for updating an item
 @app.route('/api/items/<int:index>', methods=['PUT'])
 def update_item(index):
     data = request.json
+    log_something(f"in update_item.  Data is {data} items is now {items}.")
     items[index] = data
     return jsonify(data)
 
@@ -37,8 +76,20 @@ def update_item(index):
 # API endpoint for deleting an item
 @app.route('/api/items/<int:index>', methods=['DELETE'])
 def delete_item(index):
+    log_something(f"In delete_item.  Index is {index}")
     del items[index]
     return jsonify({'message': 'Item deleted'}), 200
+
+
+def log_something(msg: str):
+    # I could use the standard python logger, but that's too complicated for what I want to do.  I just want a simple
+    # file with messages as appropriate and with a timestamp.
+    global logfile
+    now = dt.now()
+    now_str = now.isoformat()
+
+    print(f"{now_str} {msg} ", file=logfile)
+    logfile.flush()
 
 
 if __name__ == '__main__':
@@ -80,4 +131,11 @@ if __name__ == '__main__':
     </body>    
 
         """
+    now = dt.now()
+    now_str = now.isoformat()
+    # filename = LOGFILENAME + now_str + ".txt"
+    filename = LOGFILENAME + ".txt"
+    logfile = open(filename, "a+")
+    print(f"To stderr: logfilename  is {filename} ", file=sys.stderr)
+    log_something("****** Server starting ************")
     app.run(debug=True)
